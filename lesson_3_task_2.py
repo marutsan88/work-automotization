d5a8b6335d0c765b4e8d9b2af2b2f082cfcc2f0e
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-06-15 21:10:25 +0300

    Update lesson_3_task_2.py

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -3,9 +3,9 @@ from smartphone import Smartphone
 catalog = []
 catalog.append(Smartphone("Samsung", "Galaxy S21", "+790145689"))
 catalog.append(Smartphone("Xiaomi", "Mi 5", "+7931543490"))
-catalog.append(Smartphone("Google", "TECHNO SPARK 9 pro", "+7954267890"))
-catalog.append(Smartphone("OnePlus", "8 Pro", "+7911456525"))
-catalog.append(Smartphone("Apple", "iPhone 13pro", "+7934567890"))
+catalog.append(Smartphone("TECHNO", "TECHNO SPARK 9 pro", "+7954267890"))
+catalog.append(Smartphone("Infinix", "Infinix Smart 8 Pro", "+7911456525"))
+catalog.append(Smartphone("Apple", "iphone 13pro max", "+7934567890"))
 
 for phone in catalog:
     print(f"{phone.brand}, {phone.model}, {phone.phone_number}")
