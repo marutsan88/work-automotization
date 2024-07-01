7b3082b2d38704f9400f78c7a085303f3072ec44
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-07-01 09:19:36 +0300

    Delete lesson_2_task_4.py

--- lesson_2_task_4.py
+++
@@ -1,8 +0,0 @@
-import math
-def square(side):
-    area = side ** 1
-    rounded_area = math.ceil(area)
-    return rounded_area
-side_length = 5
-area = square(side_length)
-print(f"Площадь квадрата со стороной {side_length}x{area}")
