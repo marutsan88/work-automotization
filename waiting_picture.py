321b8a9fa7ce790bdc9ca2c9118e6265dace44a2
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-07-06 11:30:20 +0300

    Update waiting_picture.py

--- waiting_picture.py
+++ waiting_picture.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")# Перейдите на страницу
 
-element = WebDriverWait(driver, 10).until(
+element = WebDriverWait(driver, 12).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "#landscape"))# обязательно дождаться появления 4-й картинки
 )
 
