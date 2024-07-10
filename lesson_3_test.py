10e4e87232e2d83832687dde157d23a7ffe520dd
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-07-10 09:06:32 +0300

    Update lesson_3_test.py

--- lesson_3_test.py
+++ lesson_3_test.py
@@ -2,6 +2,7 @@
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
