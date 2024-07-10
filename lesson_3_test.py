316d86f0e11a9bd8edb60de07963e1cab04cbc89
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-07-10 09:04:13 +0300

    Update lesson_3_test.py

--- lesson_3_test.py
+++ lesson_3_test.py
@@ -1,12 +1,11 @@
+
+
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
-from webdriver_manager.chrome import ChromeDriverManager
-
 driver = webdriver.Chrome(service=ChromeService
                           (ChromeDriverManager().install()))
 from time import sleep
