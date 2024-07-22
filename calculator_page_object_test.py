d422a34d0d63bba800a69f80e31c9d70e37094e4
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-07-22 09:09:03 +0300

    Update calculator_page_object_test.py

--- calculator_page_object_test.py
+++ calculator_page_object_test.py
@@ -1,1 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 
+from webdriver_manager.chrome import ChromeDriverManager
+from Page.CalculatorPage import CalculatorPage
+
+
+def test_form_calculator():
+    
+        driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+
+
+        calculator_page = CalculatorPage(driver)
+
+        calculator_page.delay()
+        calculator_page.sum_of_the_numbers()
+        calculator_page.get_result()
+        calculator_page.close_driver()
