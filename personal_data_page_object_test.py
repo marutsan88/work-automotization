cdddbe4c624ab2dcfde73593b191a89c8b2c265e
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-07-22 09:10:22 +0300

    Update personal_data_page_object_test.py

--- personal_data_page_object_test.py
+++ personal_data_page_object_test.py
@@ -1,1 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 
+from webdriver_manager.chrome import ChromeDriverManager
+
+from Page.PersonalDataPage import PersonalDataPage
+
+
+def test_form_elements():
+  
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+    
+        personal_data_page = PersonalDataPage(driver)
+
+        personal_data_page.personal_data("Иван", "Петров", "Ленина, 55-3", "test@skypro.com", "+7985899998787", "Москва", "Россия", "QA", "SkyPro")
+        personal_data_page.zip_code_red()
+        personal_data_page.other_fields_green()
+        personal_data_page.close_driver()
