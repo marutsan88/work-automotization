7151b9f65ba9247d506e13d527dc4fa95b5d1ffa
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-08-09 19:27:14 +0300

    Update and rename test_xclient_employee.py to test_client_company.py

--- work 9/test_client_company.py
+++ work 9/test_client_company.py
@@ -14,15 +14,15 @@ db_connection_string = ('postgresql://x_clients_db_3fmx_user:'
 company_table = CompanyTable(db_connection_string)
 employee_table = EmployeeTable(db_connection_string)
 
-# New company
+# Новая компания
 name = 'NewCompany4'
 description = 'My new company 4'
 
-# New employee
+# Новый сотрудник
 id = 1
-first_name = "Ilya4"
-last_name = "Ilin"
-middle_name = "Ilich"
+first_name = "Александр"
+last_name = "Семирунний"
+middle_name = "Игоревич"
 email = "new@gmail.com"
 employee_url = "url1.com"
 phone = "+79998887766"
@@ -30,27 +30,27 @@ birthdate = "2000-06-07T08:06:30.137Z"
 is_active = True
 
 
-@allure.title('Get employee list')
-@allure.description('Test get employee list by API')
-@allure.feature('Employee')
+@allure.title('Получить список сотрудников')
+@allure.description('Тест получения списка сотрудников с помощью API')
+@allure.feature('Сотрудник')
 @allure.severity(allure.severity_level.NORMAL)
 def test_get_employee_list():
-    # create new company in DB
+    # создать новую компанию в DB
     company_table.create(name, description)
     new_company_id = company_table.get_max_id()
 
-    # create new employee in DB
+    # создать нового сотрудника в DB
     employee_table.create(first_name, last_name, phone, new_company_id,
                           is_active)
     new_employee_id = employee_table.get_max_id()
 
-    # get employee list by API
+    # получить список сотрудников с помощью API
     employee_list_api = employee_api.get_employee_list(new_company_id)
 
-    # get employee list by DB
+    # получить список сотрудников в DB
     employee_list_db = employee_table.get_company_employees(new_company_id)
 
-    # delete new employee and new company from DB
+    # удалить нового сотрудника и новую компанию из DB
     employee_table.delete(new_employee_id)
     company_table.delete(new_company_id)
 
@@ -61,77 +61,77 @@ def test_get_employee_list():
         assert len(employee_list_api) == len(employee_list_db)
 
 
-@allure.title('Create employee')
-@allure.description('Test create employee by API')
-@allure.feature('Employee')
+@allure.title('Создать сотрудника')
+@allure.description('Тест создания сотрудника с помощью API')
+@allure.feature('Сотрудник')
 @allure.severity(allure.severity_level.CRITICAL)
 def test_create_employee():
-    # create new company in DB
+    # создать новую уомпанию в DB
     company_table.create(name, description)
     new_company_id = company_table.get_max_id()
 
-    # create new employee by API
+    # создать нового сотрудника с помощью API
     new_employee = employee_api.add_employee(id, first_name, last_name,
                                              middle_name, new_company_id,
                                              email, employee_url, phone,
                                              birthdate, is_active)
     new_employee_id = new_employee["id"]
 
-    # get employee from DB
+    # получить сотрудника из DB
     employee = employee_table.get_employee_by_id(new_employee_id)
 
-    # delete new employee and new company from DB
+    # удалить нового сотрудника и новую компанию из DB
     employee_table.delete(new_employee_id)
     company_table.delete(new_company_id)
 
-    with allure.step('Check that new employee was created in DB'):
-        assert len(employee) == 1, "Employee was not created"
+    with allure.step('Проверьте, был ли создан новый сотрудник в DB'):
+        assert len(employee) == 1, "Сотрудник не был создан"
 
 
-@allure.title('Get employee')
-@allure.description('Test employee by API')
-@allure.feature('Employee')
+@allure.title('Получить сотрудника')
+@allure.description('Протестируйте сотрудника с помощью API')
+@allure.feature('Сотрудник')
 @allure.severity(allure.severity_level.NORMAL)
 def test_get_employee():
-    # create new company in DB
+    # создать новую компанию в DB
     company_table.create(name, description)
     new_company_id = company_table.get_max_id()
 
-    # create new employee in DB
+    # создать нового сотрудника в DB
     employee_table.create(first_name, last_name, phone, new_company_id,
                           is_active)
     new_employee_id = employee_table.get_max_id()
 
-    # get employee's info by API
+    # получите информацию о сотруднике с помощью API
     employee = employee_api.get_employee(new_employee_id)
 
-    # delete new employee and new company from DB
+    # удалить нового сотрудника и новую компанию из DB
     employee_table.delete(new_employee_id)
     company_table.delete(new_company_id)
 
-    with allure.step('Check that new id return from API'):
+    with allure.step('Проверьте, возвращается ли новый идентификатор из API'):
         assert employee["id"] == new_employee_id
-    with allure.step('Check that new employee first name return from API'):
+    with allure.step('Проверьте, чтобы имя нового сотрудника возвращалось из API'):
         assert employee["firstName"] == first_name
-    with allure.step('Check response body length'):
+    with allure.step('Проверьте длину тела срабатывания'):
         assert len(employee) == 12
 
 
-@allure.title('Change employee by API')
-@allure.description('Test change employee by API')
-@allure.feature('Employee')
+@allure.title('Сменить сотрудника с помощью API')
+@allure.description('Протестируйте смену сотрудника с помощью API')
+@allure.feature('Сотрудник')
 @allure.severity(allure.severity_level.NORMAL)
 def test_change_employee_by_api():
-    # create new company in DB
+    # создать новую компанию в DB
     company_table.create(name, description)
     new_company_id = company_table.get_max_id()
 
-    # create new employee in DB
+    # создать нового сотрудника в DB
     employee_table.create(first_name, last_name, phone, new_company_id,
                           is_active)
     new_employee_id = employee_table.get_max_id()
 
-    # patch employee by API
+    # исправьте ошибку сотрудника с помощью API
     new_email = "new_new@gmail.com"
     new_url = "url2.com"
     new_is_active = False
@@ -139,50 +139,50 @@ def test_change_employee_by_api():
                                                    new_email, new_url,
                                                    new_is_active
                                                    )
-    with allure.step('Check patched employee info from API'):
+    with allure.step('Проверьте исправленную информацию о сотруднике из API'):
         assert patched_employee["id"] == new_employee_id
         assert patched_employee["email"] == new_email
         assert patched_employee["url"] == new_url
         assert patched_employee["isActive"] == new_is_active
 
-    # get patched employee's info from DB
+    # получите исправленную информацию о сотруднике из DB
     employee = employee_table.get_employee_by_id(new_employee_id)
 
-    # delete new employee and new company from DB
+    # удалить нового сотрудника и новую компанию из DB
     employee_table.delete(new_employee_id)
     company_table.delete(new_company_id)
 
-    with allure.step('Check patched employee info from DB'):
+    with allure.step('Проверьте исправленную информацию о сотруднике из DB'):
         assert employee[0][0] == new_employee_id
         assert employee[0][8] == new_email
         assert employee[0][10] == new_url
         assert employee[0][1] == new_is_active
 
 
-@allure.title('Change employee by DB')
-@allure.description('Test change employee by DB')
-@allure.feature('Employee')
+@allure.title('Изменить сотрудника по DB')
+@allure.description('Тестовое изменение сотрудника по DB')
+@allure.feature('Сотрудник')
 @allure.severity(allure.severity_level.NORMAL)
 def test_change_employee_by_db():
-    # create new company in DB
+    # создать новую компанию в DB
     company_table.create(name, description)
     new_company_id = company_table.get_max_id()
 
-    # create new employee in DB
+    # создать нового сотрудника в DB
     employee_table.create(first_name, last_name, phone, new_company_id,
                           is_active)
     new_employee_id = employee_table.get_max_id()
 
-    # patch employee by DB
+    # исправьте сотрудника из DB
     new_email = "new_new@gmail.com"
     new_url = "url2.com"
     new_is_active = False
     employee_table.update(new_employee_id, new_email, new_url, new_is_active)
 
-    # get employee's info by API
+    # получить информацию сотрудников с помощью API
     employee = employee_api.get_employee(new_employee_id)
 
-    with allure.step('Check patched employee info from API'):
+    with allure.step('Проверьте исправленную информацию о сотруднике из API'):
         assert employee["id"] == new_employee_id
         assert employee["email"] == new_email
         assert employee["avatar_url"] == new_url
